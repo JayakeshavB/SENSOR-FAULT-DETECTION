a4372a7e22731436c9c8f44e91ca61e41eb178fe
jayakeshav <99580688+JayakeshavB@users.noreply.github.com>
2022-12-18 00:14:11 -0600

    project initial setup has done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SENSOR-FAULT-DETECTION
+
+The Air pressure system (APS) is a crtical component of a heavy-duty vehicle that uses compressed air to force a piston to provide pressure to the brake pads, slowing the vehicle down. The benifits of using an APS instead of a hydraulic system are the easy availability and long term sustainability of natural air.
+
+conda create environment
+
+conda create -p venv python==3.8
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.1'
+
+setup(
+    name='sensor_fault_detector',
+    version=__version__,
+    description='Sensor Fault Detector',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/JayakeshavB/SENSOR-FAULT-DETECTION',
+    author='JayakeshavB',
+    author_email='jayakeshav10807@gmail.com',
+    packages=find_packages(),
+    )
